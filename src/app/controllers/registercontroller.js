20e9339dfeedd7b9530cabf9fe02b981039c1c9d
Kevin Willians <99736435+keviwsilva@users.noreply.github.com>
2024-03-06 11:35:35 -0300

    user document

--- src/app/controllers/registercontroller.js
+++ src/app/controllers/registercontroller.js
@@ -4,25 +4,38 @@ const mysqConnection = require("../../database");
 
 const router = express.Router();
 
-const { insertUser, checkIfEmailExists} = require("../models/registerModel")
+const { insertUser, checkIfEmailExists, detectarTipoDocumento, validarCNPJ,  validarCPF } = require("../models/registerModel")
 
 //-----------------cadastro-----------------//
 router.post('/user', async (req, res) => {
     try {
-        const { name, telefone, email, endereco, sexo, password } = req.body;
+        const { name, telefone, email, endereco, sexo, password, documento } = req.body;
 
         // Check if the email already exists
-        const emailExists = await checkIfEmailExists(email);
+        const emailExists = await checkIfEmailExists(email,documento);
 
         if (emailExists) {
             return res.status(400).json({ message: "Usúario já existente!" });
         }
 
         // Hash the password using bcrypt
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const tipoDocumento = detectarTipoDocumento(documento);
+        
+        if (tipoDocumento === 'CPF' && validarCPF(documento)) {
+            // Hash the password using bcrypt
+            const hashedPassword = await bcrypt.hash(password, 10);
+            // console.log(123)
+            // Insira o usuário no banco de dados
+            insertUser(name, email, hashedPassword, telefone, sexo, endereco, tipoDocumento, documento, res);
+        } else if (tipoDocumento === 'CNPJ' && validarCNPJ(documento)) {
+            // Hash the password using bcrypt
+            const hashedPassword = await bcrypt.hash(password, 10);
 
-        insertUser(name,  email,hashedPassword,telefone, sexo, endereco, res)
-
+            // Insira o usuário no banco de dados
+            insertUser(name, email, hashedPassword, telefone, sexo, endereco, tipoDocumento, documento, res);
+        } else {
+            return res.status(400).json({ message: "Documento inválido. Deve ser CPF ou CNPJ." });
+        }
        
     } catch (error) {
         res.status(400).json({ message: "Erro interno do servidor." });
--- src/app/models/registerModel.js
+++ src/app/models/registerModel.js
@@ -1,25 +1,56 @@
 const mysqConnection = require("../../database/index");
+const { cpf: cpfValidator, cnpj: cnpjValidator } = require('cpf-cnpj-validator');
 
-const insertUser = (name,  email,  hashedPassword, telefone, sexo, endereco) =>{
-    const insertuserquery = "INSERT INTO tbl_user(user_name, user_email, user_password, user_telefone, user_sexo, user_end  ) VALUES (?, ?, ?, ?, ?, ?)";
-    mysqConnection.query(insertuserquery, [name,  email,hashedPassword,telefone, sexo, endereco  ], (err, results) => {
+
+const insertUser = (name, email, hashedPassword, telefone, sexo, endereco, tipoDocumento, documento, response) => {
+    const insertUserQuery = "INSERT INTO tbl_user(user_name, user_email, user_password, user_telefone, user_sexo, user_end, user_type, user_cpfcnpj) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+    console.log(1)
+    mysqConnection.query(insertUserQuery, [name, email, hashedPassword, telefone, sexo, endereco, tipoDocumento, documento], (err, results) => {
         if (err) {
             console.error(err);
+            console.log(2)
             return response.status(400).json({ message: "Erro ao tentar realizar cadastro no banco de dados. Revise as informações." });
         }
 
-        if (results) {
+        if (results.affectedRows > 0) {
+            console.log(3)
             response.status(200).json({ message: "Cadastro realizado com sucesso. Faça login para acessar a plataforma!" });
+        } else {
+            response.status(400).json({ message: "Erro ao tentar realizar cadastro no banco de dados. Revise as informações." });
         }
     });
 }
 
+function validarCPF(cpf) {
+    return cpfValidator.isValid(cpf);
+}
+
+function validarCNPJ(cnpj) {
+    return cnpjValidator.isValid(cnpj);
+}
+
+const detectarTipoDocumento = (documento) =>{
+        // Remove caracteres não numéricos do documento
+        const cleanedDocumento = documento.replace(/\D/g, '');
+    
+        // Verifica o tamanho do documento para determinar se é um CPF ou CNPJ
+        if (cleanedDocumento.length === 11) {
+            return 'CPF';
+        } else if (cleanedDocumento.length === 14) {
+            return 'CNPJ';
+        } else {
+            return null; // Retorna null se o tamanho não corresponder a nenhum dos formatos
+        }
+    }
+    
+
 
 // Function to check if the email already exists
-async function checkIfEmailExists(email) {
+
+async function checkIfEmailExists(email, documento) {
     return new Promise((resolve, reject) => {
-        const emailCheckQuery = "SELECT COUNT(*) AS num_rows FROM tbl_user WHERE user_email = ?";
-        mysqConnection.query(emailCheckQuery, [email], (err, results) => {
+        const emailCheckQuery = "SELECT COUNT(*) AS num_rows FROM tbl_user WHERE user_email = ? AND user_cpfcnpj = ?";
+        mysqConnection.query(emailCheckQuery, [email, documento], (err, results) => {
             if (err) {
                 reject(err);
             } else {
@@ -28,4 +59,5 @@ async function checkIfEmailExists(email) {
         });
     });
 }
-module.exports = {  insertUser,checkIfEmailExists };
+
+module.exports = {  insertUser,checkIfEmailExists, detectarTipoDocumento, validarCPF, validarCNPJ };
